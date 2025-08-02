89836cc011dae5d4e4829a6f589f4a407e287b62
Aliou221 <a43695888@gmail.com>
2025-08-02 17:41:12 +0000

    first commit

--- challenge-frontend/vite.config.js
+++ challenge-frontend/vite.config.js
@@ -5,6 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'https://h24-code-challenge.free.nf',
+    }
+  },
   plugins: [
     react(),
     tailwindcss(),
