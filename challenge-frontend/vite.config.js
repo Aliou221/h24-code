b925cb59ec9e65ec79caca4417838b305079c7d8
ALIOU CISSE <143758558+Aliou221@users.noreply.github.com>
2025-08-03 15:54:33 +0000

    Update vite.config.js

--- challenge-frontend/vite.config.js
+++ challenge-frontend/vite.config.js
@@ -5,11 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': 'https://h24-code-challenge.free.nf',
-    }
-  },
   plugins: [
     react(),
     tailwindcss(),
